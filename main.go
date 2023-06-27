bde32518ea894cdb99b0e74fc7cd16da7e5f0a20
Abhishek Ghimire <abhishek@pop-os.localdomain>
2023-06-27 13:00:18 +0545

    simple server made that always responds with status code 404

--- main.go
+++ main.go
@@ -2,8 +2,31 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 )
 
+func middlewareCors(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS,PUT,DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "*")
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
+}
+
 func main() {
-	fmt.Println("This is a web server in go for chirpy!")
+	mux := http.NewServeMux()
+	corsMux := middlewareCors(mux)
+	server := http.Server{
+		Handler: corsMux,
+	}
+	server.Addr = ":8080"
+	err := server.ListenAndServe()
+	if err != nil {
+		fmt.Println("Error Startng server: ", err)
+	}
 }
